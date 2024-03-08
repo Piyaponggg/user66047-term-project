bc768e7e17f48379ff98ae297d70b1ecce5830c1
admin <admin@admins-iMac.local>
2024-03-08 10:26:37 +0700

    first commit

---
+++ Step2/server/dbconfig.js
@@ -0,0 +1,19 @@
+var dbconfig = {
+    development: {
+        //connectionLimit : 10,
+        host: 'localhost',
+        port: '3306',
+        user: 'root',
+        password: '',
+        database: 'test1'
+    },
+    production: {
+        //connectionLimit : 10,
+        host: 'localhost',
+        port: '3306',
+        user: 'root',
+        password: '',
+        database: 'test1'
+    }
+};
+module.exports = dbconfig;
